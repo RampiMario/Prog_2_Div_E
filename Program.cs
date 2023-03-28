e0d3d856e02f658adeb2e4afcd3257fb063ece1f
Mario Rampi <13686838+MarioAr@users.noreply.github.com>
2023-03-28 19:45:56 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Div_E
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //int numero = int.Parse(Console.ReadLine());
+
+            bool rtaParse = int.TryParse(Console.ReadLine(), out int resultado);
+
+            if (rtaParse)
+            {
+                Console.WriteLine("Funciono el parseo");
+            }
+            else
+            {
+                Console.WriteLine("No Funciono el parseo");
+            }
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
